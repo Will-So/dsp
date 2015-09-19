cf5a20c5b44f520ccba217b0a00cb352342ba71a
Will-So <ryan.d.sorenson@gmail.com>
2015-09-20 00:42:44 +0200

    finished all but 3 stats problems

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,30 @@
 # Hint:  use Google to find python function
 
+import arrow
+def day_difference(d1, d2, style='MM-DD-YYYY'):
+    """Calculates the differences between dates d1 and d2 assuming American date system. 
+    """
+    d1 = arrow.get(d1,  style)
+    d2 = arrow.get(d2, style)
+    return abs((start-stop).days)
+
+
+
 ####a) 
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
+
+day_difference(date_start, date_stop, 'MM-DD-YYYY')
+
 
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+day_difference(date_start, date_stop, 'MMDDYYYY')
+
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+day_difference(date_start, date_stop, style=None)
--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -5,13 +5,22 @@
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
 
-import csv
+import pandas as pd
 
-  def read_data(data):
-   # COMPLETE THIS FUNCTION
+def read_data(data):
+    return pd.read_csv(data)
 
-  def get_min_score_difference(self, parsed_data):
-    # COMPLETE THIS FUNCTION
+def get_min_score_difference(parsed_data):
+    """
+    """
+    differences = parsed_data.Points - parsed_data['Goals Allowed']
+    return min(abs(differences))
 
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
+def get_team(parsed_data):
+    """Returns the team with the minimum score difference. 
+    """
+    parsed_data['differences'] = parsed_data.Points - parsed_data['Goals Allowed']
+    
+    # .values[0] necessary to return a pure string rather than the array or dataframe
+    return parsed_data[parsed_data.differences == index_value].Team.values[0]
+    
